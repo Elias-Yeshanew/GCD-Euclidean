9647c81e5a650b2b425856c1e75956acf96a3412
Peter Ezzat <pete_ezzat@hotmail.com>
2018-02-17 20:10:31 +0200

    Fixed more than 2 input bug. Line 19: n1 = n2

--- euclidean_gcd.py
+++ euclidean_gcd.py
@@ -2,20 +2,22 @@ def euclidean_gcd(numbers):
 	numbers.sort()
 	numbers.reverse()
 	n1 = numbers[0]
-	
+
 	for number in range(1, len(numbers)):
 		n2 = numbers[number]
 		r = n1 % n2
-		
+
 		if r == 1:
 			n2 = 1
 			break
-		
+
 		while r != 0:
 			n1 = n2
 			n2 = r
 			r = n1 % n2
-			
+
+		n1 = n2
+
 	print(n2, '\n')
 
 #.#.#.
